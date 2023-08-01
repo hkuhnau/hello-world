0937a3c374c2230a4ca9431444c10faafb24ad5d
Heidi Kuhnau <95493237+hkuhnau@users.noreply.github.com>
2023-08-01 14:31:00 -0500

    fixing tailwind version errors
    Might need to go back and re add shadows correctly

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,16 +5,19 @@ export const metadata = {
     description: 'Discover and share AI prompts'
 }
 
-const layout = () => {
+const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
         <body>
             <div className='main'>
                 <div className='gradient'/>
             </div>
+            <main className='app'>
+              {children}
+            </main>
         </body>
     </html>
   )
 }
 
-export default layout
+export default RootLayout
