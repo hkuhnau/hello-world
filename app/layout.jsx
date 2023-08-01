1b3d9484d8894a6e7ed3d07efb74b139c11349f3
Heidi Kuhnau <95493237+hkuhnau@users.noreply.github.com>
2023-08-01 13:56:52 -0500

    Initial set up
    initial set up

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: 'Promptopia',
+    description: 'Discover and share AI prompts'
+}
+
+const layout = () => {
+  return (
+    <html lang='en'>
+        <body>
+            <div className='main'>
+                <div className='gradient'/>
+            </div>
+        </body>
+    </html>
+  )
+}
+
+export default layout
---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+//import react is no longer needed in nextjs
+//rafce returns a react arrow function
+
+const Home = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
