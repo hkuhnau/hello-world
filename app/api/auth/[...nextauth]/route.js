105c107fe2a67da443659828bce66b12d752ce7c
Heidi Kuhnau <95493237+hkuhnau@users.noreply.github.com>
2023-10-07 09:58:44 -0500

    Navigation, User login and Sign out

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import { connectToDB } from '@utils/database';
+import User from '@models/user';
 
 const handler = NextAuth({
     providers: [
@@ -9,19 +10,35 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    async session ({session}){
-        
-    },
-    async signIn({profile}){
-        try{
-            //serverless -> lambda -> dynamodb (everytime it's called it needs to open up a server and make connection to a database)
-            await connectToDB();
-            //check if a user already exists
-            //if not, create a new user
-            return true;
-        } catch(error){
-            console.log(error);
-            return false;
+    callbacks: {
+        async session ({session}){
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            })
+            session.user.id = sessionUser._id.toString();
+            return session;
+        },
+        async signIn({profile}){
+            try{
+                //serverless -> lambda -> dynamodb (everytime it's called it needs to open up a server and make connection to a database)
+                await connectToDB();
+                //check if a user already exists
+                const userExists = await User.findOne({
+                    email: profile.email
+                });
+                //if not, create a new user
+                if(!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+                return true;
+            } catch(error){
+                console.log(error);
+                return false;
+            }
         }
     }
 })
