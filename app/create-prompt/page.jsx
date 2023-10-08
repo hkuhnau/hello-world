cc8c0eadeca41621e493479d812b9f680aac901e
Heidi Kuhnau <95493237+hkuhnau@users.noreply.github.com>
2023-10-08 15:43:05 -0500

    built the form page

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,52 @@
+'use client'; // when we import a hook, we use 'use client'
+
+import {useState} from 'react'
+import {useSession} from 'next-auth/react' //this allows us to know which user is logged in
+import { Router, useRouter } from 'next/router';
+import Form from '@components/Form';
+
+const CreatePrompt = () => {
+  const [submitting, setSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: ''
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try{
+      const response = await fetch('/api/prompt/new',
+        {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt, 
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      })
+
+      if(response.ok) {
+        Router.push('/');
+      }
+
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false)
+    }
+  }
+
+  return (
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  )
+}
+
+export default CreatePrompt 
